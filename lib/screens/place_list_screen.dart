8ff45e74054e00a0ec85ad95cf336007d995ddff
Allen <allenmcndounger@gmail.com>
2019-10-13 10:26:57 +0300

    Using future builder to render the fetched Data from the database

--- lib/screens/place_list_screen.dart
+++ lib/screens/place_list_screen.dart
@@ -19,37 +19,49 @@ class PlaceListScreen extends StatelessWidget {
           )
         ],
       ),
-      body: Consumer<GreatPlace>(
-        child: Column(
-          children: <Widget>[
-            Center(
-              child: Text('Got no places now.. add some'),
-            ),
-            SizedBox(height: 10),
-            FlatButton.icon(
-              icon: Icon(Icons.add),
-              label: Text('Click here, to add Place'),
-              textColor: Theme.of(context).primaryColor,
-              onPressed: () {
-                Navigator.of(context).pushNamed(AddPlaceScreen.routeName);
-              },
-            ),
-          ],
-        ),
-        builder: (context, greatPlace, ch) => greatPlace.items.length <= 0
-            ? ch
-            : ListView.builder(
-                itemCount: greatPlace.items.length,
-                itemBuilder: (ctx, i) => ListTile(
-                  leading: CircleAvatar(
-                    backgroundImage: FileImage(greatPlace.items[i].image),
+      body: FutureBuilder(
+        future:
+            Provider.of<GreatPlace>(context, listen: false).fetchAndSetPlaces(),
+        builder: (ctx, snapShot) =>
+            snapShot.connectionState == ConnectionState.waiting
+                ? Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : Consumer<GreatPlace>(
+                    child: Column(
+                      children: <Widget>[
+                        Center(
+                          child: Text('Got no places now.. add some'),
+                        ),
+                        SizedBox(height: 10),
+                        FlatButton.icon(
+                          icon: Icon(Icons.add),
+                          label: Text('Click here, to add Place'),
+                          textColor: Theme.of(context).primaryColor,
+                          onPressed: () {
+                            Navigator.of(context)
+                                .pushNamed(AddPlaceScreen.routeName);
+                          },
+                        ),
+                      ],
+                    ),
+                    builder: (context, greatPlace, ch) =>
+                        greatPlace.items.length <= 0
+                            ? ch
+                            : ListView.builder(
+                                itemCount: greatPlace.items.length,
+                                itemBuilder: (ctx, i) => ListTile(
+                                  leading: CircleAvatar(
+                                    backgroundImage:
+                                        FileImage(greatPlace.items[i].image),
+                                  ),
+                                  title: Text(greatPlace.items[i].title),
+                                  onTap: () {
+                                    //go to detail page
+                                  },
+                                ),
+                              ),
                   ),
-                  title: Text(greatPlace.items[i].title),
-                  onTap: () {
-                    //go to detail page
-                  },
-                ),
-              ),
       ),
     );
   }
