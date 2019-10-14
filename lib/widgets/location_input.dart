cd0a27cfc346db731e968c9a03833e850bcebe47
Allen <allenmcndounger@gmail.com>
2019-10-14 13:20:13 +0300

    Location Input

--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:location/location.dart';
 
+import '../helpers/location_helper.dart';
+
 class LocationInput extends StatefulWidget {
   @override
   _LocationInputState createState() => _LocationInputState();
@@ -11,8 +13,12 @@ class _LocationInputState extends State<LocationInput> {
 
   Future<void> _getCurrentUserLocation() async {
     final locData = await Location().getLocation();
-    print(locData.latitude);
-    print(locData.longitude);
+    final staticImageUrl = LocationHelper.generateLocationImagePreview(
+        latitude: locData.latitude, longitude: locData.longitude);
+
+    setState(() {
+      _previewUrl = staticImageUrl;
+    });
   }
 
   @override
@@ -31,6 +37,7 @@ class _LocationInputState extends State<LocationInput> {
               : Image.network(
                   _previewUrl,
                   fit: BoxFit.cover,
+                  width: double.infinity,
                 ),
         ),
         Row(
